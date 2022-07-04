50f17b695ba389c0522cf184f5a8ff11075c0162
Hiep Nguyen <gagga5376@gmail.com>
2022-07-04 15:02:35 +0700

    BaiTap3

--- src/BaiTap2.java
+++ src/BaiTap2.java
@@ -7,20 +7,15 @@ public class BaiTap2 {
         int num = 3;
         System.out.println("First twenty prime numbers are: ");
         System.out.println(2);
-        for ( int i = 2 ; i <= 20;)
-        {
-            for ( int j = 2 ; j <= Math.sqrt(num) ; j++ )
-            {
-                if ( num%j == 0 )
-                {
+        for ( int i = 2 ; i <= 20;i++) {
+            for ( int j = 2 ; j <= Math.sqrt(num) ; j++) {
+                if ( num%j == 0 ) {
                     status = 0;
                     break;
                 }
             }
-            if ( status != 0 )
-            {
+            if ( status != 0 ) {
                 System.out.println(num);
-                i++;
                 count++;
             }
             status = 1;
